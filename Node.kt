52944a725280ea0ff2910e6720941b61654d3e42
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-10-12 22:27:43 +0900

    Node.kt 주석 스타일 변경

--- Node.kt
+++ Node.kt
@@ -1,23 +1,29 @@
-// 제네릭 타입 노드를  정의한 클래스
-// data class : 코틀린에서 제공하는 특별한 클래스, 주로 데이터를 저장, 전달
-// equals(), hashCode(), toString(), copy(), componentN() 등을 기본 제공한다.
-data class Node<T> (var value : T, var next : Node<T>? = null){  // 각 노드는 value 라는 값을 저장하고 다음 노드에 대한 링크를 갖는다
-    // 마지막 노드는 다음 노드정보에 널값이 할당된다.
-    override fun toString(): String {  // toString() 함수는 모든 코틀린 클래스에서 기본적으로 제공하는 메서드
-        // 오버라이드 후 노드들의 연결상태를 읽기 쉽게 출력하는 함수로
-        return if (next != null) {   // 다음 노드정보가 널이 아니라면 toString()함수를 다시 호출
+// 제네릭 타입 노드를 정의한 클래스
+// 각 노드는 값을 저장하고, 다음 노드를 가리키는 링크를 갖는다
+data class Node<T>(var value: T, var next: Node<T>? = null) {
+
+    // 노드들의 연결 상태를 문자열로 출력하는 메서드
+    // 시간 복잡도: O(n)
+    override fun toString(): String {
+        // 다음 노드가 있으면 재귀 호출
+        return if (next != null) {
+            // 코틀린의 문자열 출력에서 $변수 뿐만 아니라 $함수의 반환값도 가능하다. 
             "$value -> ${next.toString()}"
-        } else {   // 다음 노드정보가 널이라면 마지막 노드
+            // 다음 노드가 없다면 재귀 호출X 
+        } else {
             "$value"
         }
     }
-    fun printInReverse() {  // 리스트를 역순으로 출력하는 함수
-        this.next?.printInReverse()  //다음 노드가 존재하면 재귀적으로 호출하여 끝까지 간 다음 출력
-        // next 가 널값이라면 해당 코드는 실행되지 않는다.
-        if (this.next != null) { // 다음 노드가 널값이 아니라면.
-            print(" -> ")  // 화살표 출력
-        }
 
-        print(this.value.toString()) // 현재 노드를 출력
+    // 리스트를 역순으로 출력하는 메서드
+    // 시간 복잡도: O(n)
+    fun printInReverse() {
+        // 재귀적으로 다음 노드를 출력한 후 현재 노드 출력
+        // ?. 앞의 값이 널이라면 ?. 뒤에 값은 실행되지 않는다. 
+        this.next?.printInReverse()
+        if (this.next != null) {
+            print(" -> ")
+        }
+        print(this.value.toString())
     }
 }
